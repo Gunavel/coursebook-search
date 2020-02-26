2ff2ed045d0e5166c9bb490ff492503a62f71280
Guna <sgb.rated@gmail.com>
2020-02-26 11:43:18 +0530

    Added Search and findMatches method with spec

--- src/course-book-finder.js
+++ src/course-book-finder.js
@@ -1,9 +1,60 @@
 const MIN_MATCH_CHAR_LENGTH = 2;
 const DEFAULT_SUGGESTIONS_COUNT = 3;
+const MIN_COLLECTION_LENGTH_PER_ITERATION = 10000;
 
 class CourseBookFinder {
-  constructor(summaries) {
-    this.collection = summaries;
+  constructor(data) {
+    this.courseBooksData = data;
+    this.summariesCollection = data.summaries;
+  }
+
+  search(query, suggestionsCount) {
+    if (query.length < MIN_MATCH_CHAR_LENGTH) {
+      return [];
+    }
+
+    const rawQuery = String(query).toLowerCase();
+    const words = rawQuery.split(" ");
+
+    return this._findMatches(rawQuery, words);
+  }
+
+  _findMatches(rawQuery, words) {
+    const collection = this.summariesCollection;
+    const exactMatches = [];
+    const partialMatches = [];
+
+    return new Promise(resolve => {
+      for (let i = 0, len = collection.length; i < len; i++) {
+        const text = String(collection[i].summary).toLowerCase();
+
+        if (text === rawQuery) {
+          exactMatches.push({
+            summary_id: i
+          });
+          break;
+        }
+
+        let score = 0;
+
+        for (let j = 0, wordsLength = words.length; j < wordsLength; j++) {
+          const word = words[j];
+          score = score + text.split(word).length - 1;
+        }
+
+        partialMatches.push({
+          summary_id: i,
+          score
+        });
+      }
+
+      const result = {
+        exactMatches,
+        partialMatches
+      };
+
+      resolve(result);
+    });
   }
 }
 
--- test/course-book-finder.test.js
+++ test/course-book-finder.test.js
@@ -1,10 +1,83 @@
 const mockData = require("../mock/data.json");
 const CourseBookFinder = require("../src/course-book-finder");
 
-describe("CourseBookFinder", () => {
+describe("CourseBookFinder: constructor", () => {
   test("should throw error when called as a function", () => {
     expect(() => {
       CourseBookFinder();
     }).toThrow();
+  });
+
+  test("should not throw error when called with new keyword", () => {
+    expect(() => {
+      const finder = new CourseBookFinder(mockData);
+    }).not.toThrow();
+  });
+});
+
+describe("CourseBookFinder: search", () => {
+  let finder;
+  beforeEach(() => {
+    finder = new CourseBookFinder(mockData);
+  });
+
+  afterAll(() => {
+    finder = undefined;
+  });
+
+  test("should return empty array if query length is less than min char length", () => {
+    finder._findMatches = jest.fn();
+    const result = finder.search("a");
+
+    expect(result).toEqual([]);
+    expect(finder._findMatches).not.toHaveBeenCalled();
+  });
+
+  test("should call _findMatches function", () => {
+    finder._findMatches = jest.fn();
+    finder.search("as");
+
+    expect(finder._findMatches).toHaveBeenCalled();
+  });
+
+  test("should call _findMatches function with arguments", () => {
+    finder._findMatches = jest.fn();
+    finder.search("is your problems");
+
+    expect(finder._findMatches).toHaveBeenCalledWith("is your problems", [
+      "is",
+      "your",
+      "problems"
+    ]);
+  });
+});
+
+describe("CourseBookFinder: _findMatches", () => {
+  let finder;
+  beforeEach(() => {
+    finder = new CourseBookFinder(mockData);
+  });
+
+  afterAll(() => {
+    finder = undefined;
+  });
+
+  test("should have exactMatches and partialMatches property in the result", done => {
+    finder
+      ._findMatches("is your problems", ["is", "your", "problems"])
+      .then(result => {
+        expect(result).toHaveProperty("exactMatches");
+        expect(result).toHaveProperty("partialMatches");
+        done();
+      });
+  });
+
+  test("should have exactMatch if raw query matches", done => {
+    const rawQuery =
+      "The Book in Three Sentences: Seek out new ideas and try new things. When trying something new, do it on a scale where failure is survivable. Seek out feedback and learn from your mistakes as you go along.";
+    finder._findMatches(rawQuery, []).then(result => {
+      expect(result.exactMatches).toEqual([{ summary_id: 0 }]);
+      done();
+    });
   });
 });
