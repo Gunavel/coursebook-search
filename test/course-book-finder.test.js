48911f68e7c8cd740df06b6ce1c29ac8a8d4a57b
Guna <sgb.rated@gmail.com>
2020-02-26 12:59:27 +0530

    100% test coverage

--- test/course-book-finder.test.js
+++ test/course-book-finder.test.js
@@ -280,4 +280,41 @@ describe("CourseBookFinder", () => {
       done();
     });
   });
+
+  test("should find coursebooks for the given query string and count", done => {
+    const result = [
+      {
+        author: "Mark Manson",
+        summary:
+          "The Book in Three Sentences: Finding something important and meaningful in your life is the most productive use of your time and energy. This is true because every life has problems associated with it and finding meaning in your life will help you sustain the effort needed to overcome the particular problems you face. Thus, we can say that the key to living a good life is not giving a fuck about more things, but rather, giving a fuck only about the things that align with your personal values.",
+        title: "Slipstream Time Hacking"
+      }
+    ];
+    const query = "is your problems";
+    const limit = 1;
+
+    finder.search(query, limit).then(books => {
+      expect(books).toEqual(result);
+      done();
+    });
+  });
+
+  test("should have exactMatch if raw query matches", done => {
+    const result = [
+      {
+        author: "Grant Cardone",
+        summary:
+          "The Book in Three Sentences: The 10X Rule says that 1) you should set targets for yourself that are 10X greater than what you believe you can achieve and 2) you should take actions that are 10X greater than what you believe are necessary to achieve your goals. The biggest mistake most people make in life is not setting goals high enough. Taking massive action is the only way to fulfill your true potential.",
+        title: "The Richest Man in Babylon"
+      }
+    ];
+    const query =
+      "The Book in Three Sentences: The 10X Rule says that 1) you should set targets for yourself that are 10X greater than what you believe you can achieve and 2) you should take actions that are 10X greater than what you believe are necessary to achieve your goals. The biggest mistake most people make in life is not setting goals high enough. Taking massive action is the only way to fulfill your true potential.";
+    const limit = 1;
+
+    finder.search(query, limit).then(books => {
+      expect(books).toEqual(result);
+      done();
+    });
+  });
 });
